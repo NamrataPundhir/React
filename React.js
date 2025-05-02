3bb46fd0990bd3899c0de99e59fd4d2e4949678d
NamrataPundhir <namratapundhir091@gmail.com>
2025-05-02 12:44:39 +0530

    FIrst commit

---
+++ React.js
@@ -0,0 +1,8 @@
+//React is a single page application(spa)
+//most popular js library for creating ui
+//also used by netflix and instagram
+//we can build modern websites and applications
+//WHAT IS LIBRARY
+//a library in programming can be explain as a collection of codes.We use  a library from it into our projects.jQuery is a library for example.
+//we can write js much simpler by using jQuery.
+//ANGULAR is a framework for this.
